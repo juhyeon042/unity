44db375acfcb0b85daf701ed453b50d941151f1d
juhyeon042 <72910402+juhyeon042@users.noreply.github.com>
2020-11-25 17:53:34 +0900

    소프트웨어 전시회(피하기 게임)
    미완성입니다.
    메인카메라 위치잡는게 어려웠습니다.

---
+++ New Unity Project/Assets/CODE/m_player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class m_player : MonoBehaviour
+{
+
+    int speed = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaMove = speed * Time.deltaTime;
+        float keyForward = Input.GetAxis("Vertical");
+        float keySide = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * deltaMove * keyForward);
+        transform.Translate(Vector3.right * deltaMove * keySide);
+        
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
+    }
+}
